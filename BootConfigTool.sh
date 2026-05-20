17a8960c7e3d90cc6acda77dfbdbdb8007cf03a0
RileyMarquis3 <15660294+RileyMarquis3@users.noreply.github.com>
2026-05-19 22:54:01 -0500

    Fix conditional check for functions file existence

--- BootConfigTool.sh
+++ BootConfigTool.sh
@@ -7,13 +7,14 @@
 BCT_FuncFile="$(pwd)/BootConfigTool.bfunc"
 
 # Source Our Config File
-if -f [ "$BCT_FuncFile" ]; then 
-  # shellcheck source=/dev/null
-  source "$BCT_FuncFile"
+if [ -f "$BCT_FuncFile" ]; then
+    # shellcheck source=/dev/null
+    source "$BCT_FuncFile"
 else
-  echo "ERROR: Functions File $BCT_FuncFile Not Found!"
-  break
+    echo "ERROR: Functions File $BCT_FuncFile Not Found!"
+    break
 fi
+
 
 
 ### Main Program ###
