9c716d480b2995fc44229b4b7ae4f6983fede698
RileyMarquis3 <15660294+RileyMarquis3@users.noreply.github.com>
2026-05-19 22:51:06 -0500

    Correct file existence check for function file
    Fix condition to check if the function file exists.

--- BootConfigTool.sh
+++ BootConfigTool.sh
@@ -8,6 +8,7 @@ BCT_FuncFile="$(pwd)/BootConfigTool.bfunc"
 
 # Source Our Config File
 if -f [ "$BCT_FuncFile" ]; then 
+  # shellcheck source=/dev/null
   source "$BCT_FuncFile"
 else
   echo "ERROR: Functions File $BCT_FuncFile Not Found!"
