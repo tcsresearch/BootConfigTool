9cdcc3adf8612ccf9f2af36e5aefdcbe1ec49ca9
RileyMarquis3 <15660294+RileyMarquis3@users.noreply.github.com>
2026-05-19 23:12:07 -0500

    Refactor function to source config file

--- BootConfigTool.sh
+++ BootConfigTool.sh
@@ -6,18 +6,22 @@
 # Define our Functions File
 BCT_FuncFile="$(pwd)/BootConfigTool.bfunc"
 
-# Source Our Config File
-if [ "-f" "$BCT_FuncFile" ]; then
-    # shellcheck source=/dev/null
-    source "$BCT_FuncFile"
-else
-    echo "ERROR: Functions File $BCT_FuncFile Not Found!"
-fi
+function BCT_SourceFuncFile() {
+    # Source Our Config File
+    if [ "-f" "$BCT_FuncFile" ]; then
+        # shellcheck source=/dev/null
+        source "$BCT_FuncFile"
+    else
+        echo "ERROR: Functions File $BCT_FuncFile Not Found!"
+        break
+    fi
 
 
 
 
 ### Main Program ###
+BCT_SourceFuncFile # Run Function Within This Script
+
 BootConfigTool_DisplayBanner
 BootConfigTool_GetCurrentConfig
 
